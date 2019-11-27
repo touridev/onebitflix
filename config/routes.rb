9c32047630c6b856a7db03493e7a85d7918de7c6
touridev <ftorjob@gmail.com>
2019-11-27 19:47:42 -0300

    Generating Controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,23 @@
 Rails.application.routes.draw do
- devise_for :users
- root to: "home#index"
+  devise_for :users
+  root :to => "home#index"
+  
+  /api/v1/
+  namespace :api do
+    namespace :v1 do
+      get '/dashboard', to: 'dashboards#index', as: 'dashboard'
+      resources :favorites, path: "my_list", only: %i( index create )
+      delete '/my_list/:type/:id', to: 'favorites#destroy'
+      resources :reviews, only: [:index, :create]
+      resources :searches, path: "search", only: :index
+      resources :series, only: :show
+      resources :movies, only: :show do
+      member do
+        get '/executions', to: 'executions#show'
+        put '/executions', to: 'executions#update'    
+      end
+      end
+      resources :recommendations, only: :index
+    end
+  end
 end
